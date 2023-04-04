2cb40f5ed96da1639fa8c2a736eeae5d7969a431
Tatiana Shevchenko <125043008+Skitalica@users.noreply.github.com>
2023-04-04 12:50:31 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,18 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 277684942 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    # Преобразуем данные о сумме чеков в значения дохода
+    income = 327 + np.log(x)
+    
+    # Создаем датафрейм с данными дохода
+    df = pd.DataFrame({'income': income})
+    
+    # Оцениваем параметры распределения LogN и получаем оценку параметра alpha
+    mu = df['income'].mean()
+    sigma_sq = ((df['income'] - mu)**2).mean()
+    alpha = np.exp(mu - sigma_sq/2)
+    
+    return alpha
